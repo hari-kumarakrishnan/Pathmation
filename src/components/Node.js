4fcf322e954469fcf9dcb3b89e894f17782c4c79
Hari Kumarakrishnan <hari.kumarakrishnan@gmail.com>
2022-05-21 23:02:58 -0500

    node on click start

--- src/components/Node.js
+++ src/components/Node.js
@@ -1,8 +1,12 @@
 import React from "react"; 
 import "./Node.css";
+
 const Node = ({ isStart, isEnd, row, col, isWall}) => {
     const classes = isStart ? "node-start" : isWall ? "iswall" : isEnd ? "node-end" : "";
-    return <div className={`node ${classes}`} id={`node-${row}-${col}`}></div>;
+    return <div className={`node ${classes}`} id={`node-${row}-${col}`} onClick={setType}></div>;
 };  
 
+function setType() {
+    console.log(1);
+}
 export default Node;
